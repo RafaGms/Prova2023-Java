80b1281c81da74da55dfecb7ed8cf069f6512baa
RafaGms <faelgomes2003@gmail.com>
2023-06-20 09:30:13 -0300

    todas as dependências

---
+++ src/main/java/com/provajava/provajava2/Provajava2Application.java
@@ -0,0 +1,13 @@
+package com.provajava.provajava2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Provajava2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Provajava2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/provajava/provajava2/Provajava2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.provajava.provajava2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Provajava2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
